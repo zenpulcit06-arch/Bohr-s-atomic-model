30427e06dfa3626e12bf0e7b857a4e149ff554ff
Pulkit jain <zenpulcit06@gmail.com>
2026-03-18 21:03:24 +0530

    bug fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #this is shell script run.sh
 #!/bin/bash
-gcc Simulation.c -o simulation
+gcc Simulation.c -o simulation -lm
 echo "Enter orbit of electron"
 read orbit
 echo "$orbit" | ./simulation
