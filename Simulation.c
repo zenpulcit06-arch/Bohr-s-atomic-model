b80c8dd634fa7eac02a2d397b5309eac197098fc
Pulkit jain <zenpulcit06@gmail.com>
2026-03-19 17:06:21 +0530

    added transition and improve performance

--- Simulation.c
+++ Simulation.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define p_mass 1.67262193e-27
 #define e_mass 9.1093837e-31
@@ -20,7 +22,6 @@ struct proton
   double ay;
 };
 
-
 struct electron {
   double x_position;
   double y_position;
@@ -35,6 +36,10 @@ int main()
 {
   struct electron e1;
   struct proton p;
+
+  srand(time(NULL));
+  double decay_const = 1e13;
+  int jumped = 0;
 
 
   printf("Enter state of electron\n");
@@ -78,6 +83,9 @@ int main()
 
   for (long long i = 0; i < steps; i++)
   {
+    double probability = 1 - exp( decay_const * dt);
+    double roll = (double)rand()/RAND_MAX;
+
     e1.x_position += e1.vx*dt + 0.5*e1.ax*dt*dt;
     e1.y_position += e1.vy*dt + 0.5*e1.ay*dt*dt;
 
@@ -112,6 +120,37 @@ int main()
 
    if (i % 500 == 0) {
         printf("%lld | %.3e | %.2f eV\n", i, r, total_energy / 1.602e-19);
+    }
+
+    if (!jumped && roll < probability && e1.state >1)
+    {
+      jumped = 1;
+      int new_state = e1.state -1;
+
+      double Ei = -13.6/(e1.state * e1.state);
+      double Ef = -13.6/(new_state * new_state);
+
+      double photon_energy = Ei - Ef;
+
+      printf("Transition %d -> %d | Photon = %.2f eV \n",e1.state,new_state,photon_energy);
+
+      e1.state = new_state;
+
+      r = 0.53e-10 * new_state*new_state;
+      e1.x_position = r;
+      e1.y_position = 0;
+      e1.vx = 0;
+      e1.vy = 2.18e6/new_state;
+
+      r = hypot(e1.x_position - p.x_position, e1.y_position - p.y_position);
+      force = (k * p_charge * e_charge) / (r * r);
+
+      e1.ax = (force / e_mass) * ((e1.x_position - p.x_position) / r);
+      e1.ay = (force / e_mass) * ((e1.y_position - p.y_position) / r);
+
+      p.ax = (force / p_mass) * ((p.x_position - e1.x_position) / r);
+      p.ay = (force / p_mass) * ((p.y_position - e1.y_position) / r);
+
     }
 
   }
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #this is shell script run.sh
 #!/bin/bash
 gcc Simulation.c -o simulation -lm
+if [ $? -ne 0 ]; then
+  echo "Error" >&2
+  exit 1
+fi
 echo "Enter orbit of electron"
 read orbit
 echo "$orbit" | ./simulation
